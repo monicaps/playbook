05814324cf018cae9d729d2b03ef01df051c4cd8
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2022-05-07 00:28:13 -0500

    add ejercicio completo 2 semana 2

--- weekly_mission_2/exercises/exercise2/explorers.js
+++ weekly_mission_2/exercises/exercise2/explorers.js
@@ -59,5 +59,38 @@ const explorers = [
  }
 ];
 
-//extraer ombre con foreach
-explorers
+//extraer nombre con foreach
+console.log("Nombres de los explorers")
+explorers.forEach((explorer) => console.log(explorer.name))
+
+//mostrar el stack de cada explorer
+console.log("\nStack de los explorers")
+explorers.forEach((explorer) =>
+  console.log(`Nuestro ${explorer.name} maneja el stack ${explorer.stack}`))
+
+//creacion de lista de stack con map
+console.log("\nLista de stacks")
+const stackList= explorers.map((explorer) => explorer.stack)
+console.log(stackList)
+
+console.log("\nViajeros con js en su stack")
+const explorersJS = explorers.filter((explorer) => // esta es una función
+  explorer.stack.includes('js') // indicación para solo filtrar elementos que incluyan "js"
+)
+console.log(explorersJS)
+
+console.log("\nViajeros que vienen de la CDMX")
+const explorersCDMX=explorers.find((explorer)=> explorer.city ==='CDMX')
+console.log(explorersCDMX)
+
+console.log("\nSuma de todos los exercises_completed")
+const suma_exercise= explorers.reduce((n,m) => n + m.exercises_completed,0)
+console.log(suma_exercise)
+
+console.log("\nBandera true en exercisesFinished en frontend")
+const bandera=explorers.some((b) => b.missions.frontend.exercisesFinished === true)
+console.log("\nLa existencia de la bandera es: " + bandera)
+
+console.log("\nBandera true en isFinished del onboarding")
+const banderaOnboarding= explorers.every((explorer) => explorer.missions.onboarding.isFinished === true)
+console.log("\nLa existencia de la bandera del onboarding es: " + banderaOnboarding)
